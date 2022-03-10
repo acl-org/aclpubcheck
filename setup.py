8f6ef34a46366273ab4946cf1ba84ea14f1fae61
Ryan Cotterell <rdc42@cst.cam.ac.uk>
2022-03-10 16:06:20 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+install_requires = [
+	"json",
+	"tqdm",
+	"termcolor",
+	"pandas"
+]
+
+
+setup(
+	name="aclpublic",
+	install_requires=install_requires,
+	version="0.1",
+	scripts=[]
+)
