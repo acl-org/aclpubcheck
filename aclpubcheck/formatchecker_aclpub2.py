dc617a5ddd2544c66261dcb57bcd08c11f2cd7df
Danilo Croce <croce@info.uniroma2.it>
2022-03-23 01:07:23 +0100

    Improved the automatization of the aclpub2 check

--- aclpubcheck/formatchecker_aclpub2.py
+++ aclpubcheck/formatchecker_aclpub2.py
@@ -14,38 +14,63 @@ def main():
     global args
     parser = argparse.ArgumentParser()
     parser.add_argument('--papers_dir', default=".", help='Path of the folder containing the camera ready papers.')
-    parser.add_argument('--paper_type', choices={"short", "long", "other"},
+    parser.add_argument('--paper_type', choices={"short", "long", "other", "auto"},
                         default='long')
     parser.add_argument('--papers_yaml_path', default="papers.yaml", help='Path of input papers.yaml file.')
-    parser.add_argument('--output_log', default="output.tsv", help='Path of output TSV files with the information about problematic papers.')
+    parser.add_argument('--output_file', default="output.tsv", help='Path of output TSV files with the information about problematic papers.')
+    parser.add_argument('--output_dir', default=".", help='Path of output folder with the output JSON and PNG files.')
 
 
     args = parser.parse_args()
 
-    with open(args.output_log, 'wt') as out_file:
+    input_paper_type = args.paper_type
+
+    formatter = Formatter()
+
+    # Relaxing the margin on ONE point
+    formatter.right_offset = 5.5 #4.5
+    formatter.left_offset = 3 #2
+    formatter.top_offset = 2 #1
+
+    print_only_errors = True
+
+    with open(args.output_file, 'wt') as out_file:
         tsv_writer = csv.writer(out_file, delimiter='\t')
 
-        tsv_writer.writerow(['id', 'file', 'authors', 'emails'])
+        tsv_writer.writerow(['id', 'file', 'title', 'authors', 'emails'])
 
         with open(args.papers_yaml_path) as file:
             papers = yaml.load(file, Loader=yaml.FullLoader)
 
             for paper in papers:
                 input_file_name = paper["file"]
-                is_correct = worker(os.path.join(args.papers_dir, input_file_name), args.paper_type)
 
                 authors = paper["authors"]
                 id = paper["id"]
                 file = paper["file"]
+                title = paper["title"]
+
+                paper_type = input_paper_type
+
+                if input_paper_type == "auto":
+                    yaml_paper_type = paper["metadata"]["paper_type"]
+                    if "long" in yaml_paper_type:
+                        paper_type = "long"
+                    if "short" in yaml_paper_type:
+                        paper_type = "short"
 
                 names = []
                 emails = []
+
+                is_correct = formatter.format_check(os.path.join(args.papers_dir, input_file_name), paper_type, output_dir = args.output_dir, print_only_errors = print_only_errors)
 
                 for author in authors:
                     names.append(author["name"])
                     emails.append(author["emails"])
 
-                tsv_writer.writerow([id, file, ";".join(names), ";".join(emails)])
+                if is_correct == False:
+                    tsv_writer.writerow([id, file, title, ";".join(names), ";".join(emails)])
+                    out_file.flush()
 
 
 if __name__ == "__main__":
