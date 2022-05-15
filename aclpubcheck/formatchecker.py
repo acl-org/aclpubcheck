8d7530dd341a77e02a05097e461bf5263e05e172
Ryan Cotterell <rdc42@cst.cam.ac.uk>
2022-05-15 18:19:28 +0200

    added new ethics section title

--- aclpubcheck/formatchecker.py
+++ aclpubcheck/formatchecker.py
@@ -321,7 +321,7 @@ class Formatter(object):
         # thresholds for different types of papers
         standards = {"short": 5, "long": 9, "other": float("inf")}
         page_threshold = standards[paper_type.lower()]
-        candidates = {"References", "Acknowledgments", "Acknowledgement", "Acknowledgment", "EthicsStatement", "EthicalConsiderations", "Ethicalconsiderations", "BroaderImpact", "EthicalConcerns"}
+        candidates = {"References", "Acknowledgments", "Acknowledgement", "Acknowledgment", "EthicsStatement", "EthicalConsiderations", "Ethicalconsiderations", "BroaderImpact", "EthicalConcerns", "EthicalStatement"}
         #acks = {"Acknowledgment", "Acknowledgement"}
 
         # Find (references, acknowledgements, ethics).
